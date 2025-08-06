4769c3e541138d508f78d9760515239a4bb2c327
makzprog <makzprog@gmail.com>
2025-08-06 17:40:18 +0200

    Added more functions

--- Project Orion/my-portfolio/script.js
+++ Project Orion/my-portfolio/script.js
@@ -33,3 +33,38 @@ function placeholder4() {
 function navigate33434() {
   alert("Navigate3 to next site");
 }
+function sayHello() {
+  alert("Hello from JavaScript!");
+}
+
+
+function navigate() {
+  alert("Navigate to next site");
+}
+
+
+function placeholder() {
+  alert("used as a placeholdee");
+}
+
+function placeholder2() {
+  alert("used as a placeholdee");
+}
+
+function placeholder3() {
+  alert("used as a placeholdee");
+}
+
+function placeholder4() {
+  alert("used as a placeholdee");
+}
+
+function placeholder4() {
+  alert("used as a placeholdee");
+}
+
+
+
+function navigate33434() {
+  alert("Navigate3 to next site");
+}
